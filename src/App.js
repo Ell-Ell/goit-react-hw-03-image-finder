b1c3d768085ee748a35d3a1c0b730a0323aba2b7
Ell-Ell <AllaAkhtyrskaya@gmail.com>
2021-10-21 11:34:58 +0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Modal from './components/Modal';
 import SearchBar from './components/Searchbar/Searchbar';
 import ImageGallery from './components/ImageGallery';
-import ImageGalleryItem from './components/ImageGallery/ImageGalleryItem';
+import ImageGalleryItem from './components/ImageGalleryItem';
 import Button from './components/Button';
 import ShowErrorMessage from './components/ErrorMessage';
 import Loader from './components/Loader';
@@ -28,6 +28,8 @@ class App extends Component {
     error: null,
     largeImg: '',
     showModal: false,
+    imagesEnded: false,
+    imagesNotFiended: false,
   };
 
   //if запись стейта и get
@@ -51,12 +53,9 @@ class App extends Component {
   };
 
   openModal = event => {
-    // console.log(event.target);
-    // console.log(event.target.nodeName);
     if (event.target.nodeName === 'IMG') {
       this.toggleModal();
     }
-
     const targetImg = this.state.images.find(
       ({ id }) => id === Number(event.target.id),
     );
@@ -70,16 +69,25 @@ class App extends Component {
 
   fetchImages = () => {
     const { page, searchQuery } = this.state;
-    // объект настроек(если более двух)
+    // объект настроек (если более двух)
     const options = { searchQuery, page };
 
     fetchImage(options)
       .then(images => {
-        this.setState(prevState => ({
-          images: [...prevState.images, ...images],
-          page: prevState.page + 1,
-        }));
-        this.setState({ status: Status.PENDING });
+        if (images.length === 0) {
+          this.setState(() => ({
+            imagesEnded: true,
+            imagesNotFiended: this.state.images.length === 0,
+          }));
+        } else {
+          this.setState(prevState => ({
+            images: [...prevState.images, ...images],
+            page: prevState.page + 1,
+            imagesEnded: false,
+            imagesNotFiended: false,
+          }));
+          this.setState({ status: Status.PENDING });
+        }
       })
       .catch(error => {
         this.setState({ error, status: Status.REJECTED });
@@ -120,13 +128,22 @@ class App extends Component {
         <>
           <div className="App">
             <SearchBar onSubmit={this.handleSearchbarSubmit}></SearchBar>
+
+            {this.state.imagesNotFiended ? (
+              <ShowErrorMessage message="Images not founded. Please, try again" />
+            ) : null}
+
             <ImageGallery onClick={this.openModal}>
               <ImageGalleryItem images={images} />
             </ImageGallery>
-            {images.length > 0 && <Button onClick={this.fetchImages}></Button>}
+
+            {!this.state.imagesEnded && (
+              <Button onClick={this.fetchImages}></Button>
+            )}
             {showModal && (
               <Modal largeImg={largeImg} openModal={this.toggleModal} />
             )}
+
             <ToastContainer autoClose={3000} />
           </div>
         </>
